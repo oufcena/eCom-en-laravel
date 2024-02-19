b818b44d51dac2a1996027733e1d38960542f45b
oufcena <oufcena200@gmail.com>
2024-02-19 12:16:32 +0200

    header, footer and login page done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,9 @@
+<h3>Footer page</h3>
+
+<div class='panel panel-default'>
+    <div class='panel-body'>
+      Panel content
+    </div>
+    
+    <div class='panel-footer'>Panel footer</div>
+</div>
